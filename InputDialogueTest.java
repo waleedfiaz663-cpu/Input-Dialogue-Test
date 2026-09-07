8dbd2ed662440d4e5dd3860456d343d1c1f8acd4
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-07 21:00:56 +0500

    Add files via upload

---
+++ InputDialogueTest.java
@@ -0,0 +1,35 @@
+
+package inputdialoguetest;
+
+import javax.swing.*;
+
+public class InputDialogueTest {
+
+   
+    public static void main(String[] args) {
+        
+       //String  name;
+        //name = JOptionPane.showInputDialog("Enter your name");
+        //JOptionPane.showMessageDialog(null," hello "+name);
+        
+        String num1,num2; 
+        
+        num1 = JOptionPane.showInputDialog("Enter First Number");
+        
+        num2 = JOptionPane.showInputDialog("Enter Second Number");
+        
+        int a=Integer.parseInt(num1);
+        
+        int b=Integer.parseInt(num2);
+        
+         int sum=a+b;
+         
+         JOptionPane.showMessageDialog(null,sum);
+     
+        
+        
+        
+        
+    }
+    
+}
